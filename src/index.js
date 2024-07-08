ce4476bb2c221f01aae6276e94039c78ebd1271f
Jeff Konadu Sarpong <crazicoda@gmail.com>
2024-07-08 15:24:53 +0000

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
-
-
