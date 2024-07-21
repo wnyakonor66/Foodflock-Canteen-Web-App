5b541397df629b1877327d81ceb14991c89fa6e9
Jeff Konadu Sarpong <crazicoda@gmail.com>
2024-07-21 10:32:27 +0000

    order information

---
+++ src/Component/PropValue.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PropValue({ property, value }) {
+	return (
+		<div className="w-full flex flex-row justify-between mt-3">
+			<div className="text-sm font-semibold text-gray-500">{property}</div>
+			<div className="text-sm font-bold">{value}</div>
+		</div>
+	);
+}
--- src/vendorsPage/pages/Profile.jsx
+++ src/vendorsPage/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CatNumber from "../../Component/CatNumber";
 import InputText from "../../Component/InputText";
+import InputSelect from "../../Component/InputSelect";
+import PropValue from "../../Component/PropValue";
 
 export default function Profile() {
 	return (
@@ -38,10 +40,42 @@ export default function Profile() {
 						id={"email"}
 						placeholder={"Enter your Business Location"}
 					/>
+
+					<InputSelect
+						name="Makes Delivery"
+						id="delivery"
+						options={["Yes", "No"]}
+						value={""}
+					/>
+				</div>
+
+				<div>
+					<button className="mt-4 bg-cyan-500 hover:bg-cyan-400 text-white font-bold py-2 px-4 rounded">
+						Submit Changes
+					</button>
 				</div>
 			</div>
 
-			<div className="flex flex-col bg-white h-fit p-3 shadow-md rounded-sm w-[30%]"></div>
+			<div className="flex flex-col bg-white h-fit p-3 shadow-md rounded-sm w-[30%]">
+				<div className="flex flex-row items-center">
+					<CatNumber number={2} />
+					<span className="ml-2 font-bold">Orders Information</span>
+				</div>
+
+				<div className="mt-4 flex flex-col">
+					<PropValue property={"Processed Orders"} value={"0"} />
+					<PropValue property={"Pending Orders"} value={"0"} />
+					<PropValue property={"Cancelled Orders"} value={"0"} />
+
+					<hr className="mb-2 mt-3" />
+
+					<PropValue property={"Total Orders"} value={"0"} />
+					<PropValue property={"Total Earnings"} value={"0"} />
+					<PropValue property={"Potential Earnings"} value={"0"} />
+				</div>
+			</div>
+
+			<div></div>
 		</div>
 	);
 }
